84ae6d3cd39e9f15811fc91f617f17a515942225
BatMando <mo_teleb22@yahoo.com>
2022-04-11 20:04:51 +0200

    changing app icon

--- lib/modules/favourites/favourites.dart
+++ lib/modules/favourites/favourites.dart
@@ -21,59 +21,65 @@ class FavouritesScreen extends StatelessWidget {
       child: BlocConsumer<GetAllFavCubit, GetAllFavStates>(
         listener: (context, state) {},
         builder: (context, state) {
-          return SafeArea(
-              child: Scaffold(
-            body: ConditionalBuilderRec(
-              condition: state is GetAllFavSuccessState,
-              builder: (context) => ListView.separated(
-                  itemBuilder: (context, index) {
-                    return NewsItemFav(
-                        getAllFavouriteModel:
-                            GetAllFavCubit.get(context).getAllFavouriteModel!,
-                        context: context,
-                        index: index,
-                        function: () async {
-                          NEWSID = GetAllFavCubit.get(context)
-                              .getAllFavouriteModel!
-                              .favourites![index]
-                              .postId
-                              .toString();
+          if (state is GetAllFavSuccessState){
+            return SafeArea(
+                child: Scaffold(
+                  body: ConditionalBuilderRec(
+                    condition: state is GetAllFavSuccessState,
+                    builder: (context) => ListView.separated(
+                        itemBuilder: (context, index) {
+                          return NewsItemFav(
+                              getAllFavouriteModel:
+                              GetAllFavCubit.get(context).getAllFavouriteModel!,
+                              context: context,
+                              index: index,
+                              function: () async {
+                                NEWSID = GetAllFavCubit.get(context)
+                                    .getAllFavouriteModel!
+                                    .favourites![index]
+                                    .postId
+                                    .toString();
 
-                          NavigateTo(
-                              context,
-                              NewsDetailsScreen(GetAllFavCubit.get(context)
-                                  .getAllFavouriteModel!
-                                  .favourites![index]
-                                  .postId
-                                  .toString()));
-                        });
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return const SizedBox(
-                      height: 8,
-                    );
-                  },
-                  itemCount: GetAllFavCubit.get(context)
-                      .getAllFavouriteModel!
-                      .favourites!
-                      .length),
-              fallback: (context) => Center(
-                child: SizedBox(
-                  height: 100,
-                  width: 100,
-                  child: Shimmer.fromColors(
-                    baseColor: Colors.grey,
-                    highlightColor: defaultColor,
-                    child: Shimmer.fromColors(
-                      baseColor: Colors.grey,
-                      highlightColor: defaultColor,
-                      child: Image.asset('assets/images/logo.png'),
+                                NavigateTo(
+                                    context,
+                                    NewsDetailsScreen(GetAllFavCubit.get(context)
+                                        .getAllFavouriteModel!
+                                        .favourites![index]
+                                        .postId
+                                        .toString()));
+                              });
+                        },
+                        separatorBuilder: (BuildContext context, int index) {
+                          return const SizedBox(
+                            height: 8,
+                          );
+                        },
+                        itemCount: GetAllFavCubit.get(context)
+                            .getAllFavouriteModel!
+                            .favourites!
+                            .length),
+                    fallback: (context) => Center(
+                      child: SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: Shimmer.fromColors(
+                          baseColor: Colors.grey,
+                          highlightColor: defaultColor,
+                          child: Shimmer.fromColors(
+                            baseColor: Colors.grey,
+                            highlightColor: defaultColor,
+                            child: Image.asset('assets/images/logo.png'),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-            ),
-          ));
+                ));
+          }
+          else {
+            return Container();
+          }
+
         },
       ),
     );
